c166f2012c1c0286cb538fdd9e1b5f98f322cf11
nikolan123 <94351312+nikolan123@users.noreply.github.com>
2026-04-24 23:45:53 +0300

    add a new target

---
+++ WidgetPlayerTemplate/WidgetPlayerTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetPlayerTemplateApp.swift
+//  WidgetPlayerTemplate
+//
+//  Created by Niko on 24.04.26.
+//
+
+import SwiftUI
+
+@main
+struct WidgetPlayerTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
